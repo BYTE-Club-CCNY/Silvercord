79291ccc9658120b4cd440567b62e9c8c3a2b06f
jaynopponep <noppone.p@gmail.com>
2024-11-08 11:40:31 -0500

    LLM works properly, was very easy to implement. Using claude-3-sonnet

---
+++ llm.py
@@ -0,0 +1,85 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_anthropic import ChatAnthropic
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains.combine_documents import create_stuff_documents_chain
+# for chatting testing:
+from langchain_core.messages import HumanMessage, SystemMessage
+
+
+load_dotenv()
+API_KEY = os.getenv("OPEN_AI_KEY")
+CHROMA_PATH = "chroma"
+
+def build_rag_chain(api_key):
+    embed = OpenAIEmbeddings(
+        api_key=api_key,
+        model="text-embedding-3-large"
+    )
+    db = Chroma(
+        collection_name="silvercord",
+        embedding_function=embed,
+        persist_directory=CHROMA_PATH,
+    )
+    retriever = db.as_retriever(
+        search_type="similarity_score_threshold",
+        search_kwargs={"k": 6, "score_threshold": 0.45},
+    )
+    model = ChatAnthropic(
+        model="claude-3-sonnet-20240229",
+    )
+    context = (
+        "Given a chat history and the latest user question "
+        "which might reference context in the chat history, "
+        "formulate a standalone question which can be understood "
+        "without the chat history. Do NOT answer the question, just "
+        "reformulate it if needed and otherwise return it as is."
+    )
+    context_history = ChatPromptTemplate(
+        [
+            ("system", context),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+        model, retriever, context_history
+    )
+    query = (
+        "You are an assistant for question-answering tasks. "
+        "Use the given pieces of retrieved context to answer "
+        "the question given by the user. If you don't know the "
+        "answer, just say that you do not know. Use 5 sentences "
+        "maximum and keep the answer concise. Your answer must strictly "
+        "be based on the data and context given to you. "
+        "\n\n"
+        "{context}"
+    )
+    full_prompt = ChatPromptTemplate(
+        [
+            ("system", query),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    qna_chain = create_stuff_documents_chain(model, full_prompt)
+    rag_chain = create_retrieval_chain(history_aware_retriever, qna_chain)
+    return rag_chain
+def chat():
+    print("Start asking about professors at CCNY")
+    chat_history = []
+
+    while True:
+        query = input("You: ")
+        if query.lower() == "exit":
+            break
+        rag_chain = build_rag_chain(API_KEY)
+        result = rag_chain.invoke({"input": query, "chat_history": chat_history})
+        print(f"AI: {result['answer']}")
+        chat_history.append(HumanMessage(content=query))
+        chat_history.append(SystemMessage(content=result["answer"]))
+if __name__ == "__main__":
+    chat()
