bcb11ecc21fcab50e4b4c6f13ae9cd0510f5abd5
Abrar Habib <abrarhabib285@gmail.com>
2024-12-04 21:05:21 -0500

    /problems working

---
+++ src/commands/user/problems.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { get_problems } = require('../../../dynamo_helper.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('problems')
+        .setDescription("See the problems you've completed."),
+        
+    async execute(interaction) {
+      const server_id = interaction.guild.id;
+      const user_id = interaction.user.id;
+      const problems = await get_problems(server_id, user_id);
+      if (!problems || problems.length == 0) {
+	await interaction.reply("You haven't completed any problems yet bozo!");
+      }
+      const embed = new EmbedBuilder()
+	.setTitle("Problems")
+	.setDescription(`List of problems done by ${interaction.user.username}`)
+	.addFields({
+	  name: "Problems", value: problems.join("\n")
+	})
+      await interaction.reply({embeds: [embed]});
+    },
+};
