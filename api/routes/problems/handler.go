ff67aceb03e994f1e8e873e652994a68b1f971c7
jaynoppone <noppone.p@gmail.com>
2025-09-12 13:54:31 -0400

    /getproblems

--- api/routes/problems/handler.go
+++ api/routes/problems/handler.go
@@ -1,14 +1,18 @@
 package routes
 
 import (
+	"encoding/json"
 	"github.com/go-chi/chi/v5"
 	"github.com/supabase-community/supabase-go"
+	"log"
 	"net/http"
 )
 
 type Handler struct {
 	client *supabase.Client
 }
+
+const LEETBOARD_PROBLEMS_TABLE = "leetboard_problems"
 
 func NewHandler(client *supabase.Client) http.Handler {
 	h := &Handler{client: client}
@@ -19,5 +23,25 @@ func NewHandler(client *supabase.Client) http.Handler {
 }
 
 func (h *Handler) getProblems(w http.ResponseWriter, r *http.Request) {
+	serverID := r.URL.Query().Get("server_id")
+	userID := r.URL.Query().Get("user_id")
+	var problems []GetProblemsResponse
 
+	queryBuilder := h.client.From(LEETBOARD_PROBLEMS_TABLE)
+	querySelect := queryBuilder.
+		Select("user_id, link, problem", "1", false).
+		Eq("server_id", serverID).
+		Eq("user_id", userID)
+	_, err := querySelect.ExecuteTo(&problems)
+	if err != nil {
+		http.Error(w, "Error getting problems", http.StatusInternalServerError)
+		log.Fatal("Error: ", err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(problems)
+	if err != nil {
+		http.Error(w, "Error encoding", http.StatusInternalServerError)
+		return
+	}
 }
--- api/routes/problems/struct.go
+++ api/routes/problems/struct.go
@@ -1,1 +1,7 @@
 package routes
+
+type GetProblemsResponse struct {
+	UserID  int    `json:"user_id" validate:"required"`
+	Link    string `json:"link" validate:"required"`
+	Problem string `json:"problem" validate:"required"`
+}
