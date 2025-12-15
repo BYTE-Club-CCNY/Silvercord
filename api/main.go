47522932d573bda4090025be248a57223bfd22b4
Abrar Habib <abrar.habib@shopify.com>
2025-12-15 02:20:14 -0500

    feat: add logging to go for better debugging

--- api/main.go
+++ api/main.go
@@ -14,14 +14,19 @@ func main() {
 	supabaseUrl := os.Getenv("SUPABASE_URL_PRIVATE")
 	supabaseAnonKey := os.Getenv("SUPABASE_KEY_PRIVATE")
 
+	if supabaseUrl == "" || supabaseAnonKey == "" {
+		log.Fatal("[main] SUPABASE_URL_PRIVATE or SUPABASE_KEY_PRIVATE environment variables not set")
+	}
+
 	client, err2 := supabase.NewClient(
 		supabaseUrl,
 		supabaseAnonKey,
 		&supabase.ClientOptions{},
 	)
 	if err2 != nil {
-		log.Fatal("Error creating Supabase client: ", err2)
+		log.Fatal("[main] Error creating Supabase client: ", err2)
 	}
+
 	r := routes.SetupRoutes(client)
 
 	fmt.Println("Listening on port 8080")
